ecaaf2e6f7a1e18490b2b040d0efa1a8e01d872f
1510952971 <1510952971@users.noreply.github.com>
2026-09-01 21:15:49 +0800

    fix(ios): mark scene url callback as override

--- apps/native/ios/Runner/SceneDelegate.swift
+++ apps/native/ios/Runner/SceneDelegate.swift
@@ -2,7 +2,7 @@ import Flutter
 import UIKit
 
 class SceneDelegate: FlutterSceneDelegate {
-  func scene(_ scene: UIScene, openURLContexts URLContexts: Set<UIOpenURLContext>) {
+  override func scene(_ scene: UIScene, openURLContexts URLContexts: Set<UIOpenURLContext>) {
     for context in URLContexts {
       ShortcutBridge.shared.handle(context.url)
     }
